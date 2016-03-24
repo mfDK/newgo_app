4e2cedaeb83875f540f51cc77654f8773f75d3cd
Danny Kim <dkim7474@gmail.com>
2016-03-24 15:47:17 -0400

    Added validations to master branch for goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,7 @@
 class Goal < ActiveRecord::Base
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+  validates :title, :description, :end_date, presence: true
 
   # def fb_post_goal_complete
   # 	begin
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
   belongs_to :goal
+  validates :title, :description, :end_date, presence: true
 end
