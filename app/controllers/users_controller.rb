ee30b8b1be35aeaf723db8f65e0eaf4db09e556e
Danny Kim <dkim7474@gmail.com>
2016-08-25 13:22:10 -0400

    Started changes for Version 2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
+  get 'users/show'
+
 	root 'home#index'
 	
-  devise_for :users , :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # this controller is set for the devise users because it is being used
   # for the facebook api through the callbacks_controller
   # Using the devise gem sets up routes for new user creation
   # user sign_in and sessions as well. 
+
+  resources :users, :controller => 'users'
 
   resources :goals do
     resources :tasks do
