6c8464ee7aa38dd1a270f71fdb47a90556cc742d
Danny Kim <dkim7474@gmail.com>
2016-11-06 11:39:30 -0500

    testing git

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
   	@goals = Goal.where(user_id: current_user.id).reverse
   	# I want to see if this works or not
+
+  	@test = "Hello I am Working"
   end
 end
