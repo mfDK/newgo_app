f14c7673b84cfed051b71e774f20cab5711abf22
Danny Kim <dkim7474@gmail.com>
2016-11-06 13:38:36 -0500

    New Mac Test Complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,5 @@ class HomeController < ApplicationController
   def index
   	@goals = Goal.where(user_id: current_user.id).reverse
   	# I want to see if this works or not
-
-  	@test = "Hello I am Working"
-  	@test_branch = "I am the test branch"
   end
 end
